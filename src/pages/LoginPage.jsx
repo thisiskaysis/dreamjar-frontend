7579f010ae97f5dc9ddcf0832dc2f05be3cbfc1f
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-01-31 15:27:46 +1000

    css changes to log in page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/LoginForm/LoginForm";
 function LoginPage() {
     return (
     <div className="login-page">
-    <LoginForm />;
+    <LoginForm />
     </div>
     );
 }
