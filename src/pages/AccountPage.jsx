b49a13ddee622b5f1f7d84e7dbc4390b85c2eb92
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-01-31 16:04:42 +1000

    delete. Restart Refresh

--- src/pages/AccountPage.jsx
+++
@@ -1,5 +0,0 @@
-function AccountPage() {
-    return <h1>This is the account page when logged in. Here, you can update/edit your details, add children, create campaigns for children, and see past history.</h1>;
-}
-
-export default AccountPage;
--- src/pages/BrowseCampaigns.jsx
+++
@@ -1,5 +0,0 @@
-function BrowseCampaigns() {
-    return <h1>This is the page to browse all campaigns.</h1>;
-}
-
-export default BrowseCampaigns;
