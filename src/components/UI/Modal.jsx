cdc53694ce9dc79ea33a125329c739440af57201
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-02-14 12:40:44 +1000

    modal update glass-panel

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -17,7 +17,7 @@ function Modal({ isOpen, onClose, children }) {
 
                     {/* Modal Content */}
                     <motion.div
-                    className="modal-content"
+                    className="glass-panel"
                     initial={{ scale: 0.8, opacity: 0, y: 40 }}
                     animate={{ scale: 1, opacity: 1, y: 0 }}
                     exit={{ scale: 0.8, opacity: 0, y: 40 }}
